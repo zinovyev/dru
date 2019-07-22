cb65ee151190ea241df98fc4340640dd9b495e22
Ivan Zinovyev <vanyazin@gmail.com>
2019-07-22 23:56:47 +0300

    Use ash shell by default for project

--- lib/cmd/project.sh
+++ lib/cmd/project.sh
@@ -33,7 +33,7 @@ services:
       - ./tmp/db:/var/lib/postgresql/data
   web:
     build: .
-    command: bash -c "rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'"
+    command: ash -c "rm -f tmp/pids/server.pid && bundle exec rails s -p 3000 -b '0.0.0.0'"
     volumes:
       - .:/usr/src
       - dru1-bundle:/usr/local/bundle
